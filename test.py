debdd4a347c1e209761352654f893e6cd00d80cb
BankTHP <bang.225588z@gmail.com>
2021-04-18 20:34:05 +0700

    update book

--- test.py
+++ test.py
@@ -12,18 +12,19 @@ std_id = 62102010169
 author_id = 1
 booktitle = "database test เพิ่มค่า"
 floor = '3'
-book_id = 4
+book_id = 6
 publisher = '2021-04-01'
 borrowerdate = '2021-04-01'
 returndate = '2021-04-02'
-category= ''.split('-')
+category= '1-2-3-4-5'.split('-')
 statusBook = 0 
 stock = 10
 books = '34-36'.split('-')
-insertbook = """ SELECT cat_id FROM  categorylist WHERE cat_id NOT IN (SELECT cat_id FROM category WHERE book_id = 4)"""
-cur.execute(insertbook)
-x = cur.fetchall()
-countrow = """select cat_id from category WHERE book_id = 1"""
+# insertbook = """ SELECT cat_id FROM  categorylist WHERE cat_id NOT IN (SELECT cat_id FROM category WHERE book_id = 4)"""
+# cur.execute(insertbook)
+# x = cur.fetchall()
+# print(x)
+countrow = """select cat_id from category WHERE book_id = 6"""
 cur.execute(countrow)
 xp = cur.fetchall()
 # print(x)
@@ -38,27 +39,20 @@ xp = cur.fetchall()
 # print(len(x)+xp[0][0])
 # for i in range(len(x)+xp[0][0]):
 # print("เช็คว่าอันไหนไม่มี",list(x))
-test = list()
+js = list()
 for i in range(len(xp)):
-    test.append(str(xp[i][0]))
-print("ที่ js ส่งมา ",category)
-print("ค่าของหนังสือ",test)
-if len(category) > len(test):#insert 
-    x = list(set(category)- set(test)) 
-    for i in x :
-        xinsert = """INSERT INTO category values(%s,%s)"""
-        cur.execute(xinsert,(x[0],book_id))
-        con.commit()
-elif len(category) < len(test): #ต้องdel ออก
-    x  = list(set(test)- set(category))
-    for i in x :
-        xtest = """DELETE FROM category WHERE cat_id = %s AND book_id = %s"""
-        cur.execute(xtest,(x[0],book_id))
-        con.commit()
-
-elif len(category) == len(test):
-    print("==")
-# x1 = list(set(category)- set(test)) 
-# print(x1)
-
-
+    js.append(str(xp[i][0]))
+check = list()
+for i in range(len(js)) :
+    if(js[i] in category) :
+        category.remove(js[i]) # เอาที่ต่างกัน
+    else :
+        check.append(str(js[i]))
+for i in range(len(check)):
+    delid = """DELETE FROM category WHERE book_id = %s AND cat_id = %s """
+    cur.execute(delid,(book_id,check[i]))
+    con.commit()
+for i in range(len(category)):
+    insertid = """INSERT INTO category (book_id,cat_id) VALUES (%s,%s) """
+    cur.execute(insertid,(book_id,category[i]))
+    con.commit()
